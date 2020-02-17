e9fefb6b6a157e0fdb47cf2ad8a195c7d348aa0f
Maria Lamardo <maria@pendo.io>
2020-02-17 17:06:42 +0100

    2 - Update Metadata

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,29 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: {
+      title: 'Memory Game - Home Page',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'This is the home page for the accessible Memory Game using Vue.js.'
+        }
+      ]
+    },
     component: Home
   },
   {
     path: '/Instructions',
     name: 'Instructions',
+    meta: {
+      title: 'Memory Game - Instructions Page',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'This is the instructions page for the accessible Memory Game using Vue.js.'
+        }
+      ]
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -25,5 +43,11 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const newMetaTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+  if (newMetaTitle) document.title = newMetaTitle.meta.title;
+  next();
+});
 
 export default router
